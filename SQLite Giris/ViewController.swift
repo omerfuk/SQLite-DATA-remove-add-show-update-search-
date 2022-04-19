83c40f8b52457abb76d7a5717e72882425594f52
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-04-19 17:12:54 +0300

    Initial Commit

---
+++ SQLite Giris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SQLite Giris
+//
+//  Created by Ömer Faruk Kılıçaslan on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SQLite GirisUITests/SQLite_GirisUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SQLite_GirisUITestsLaunchTests.swift
+//  SQLite GirisUITests
+//
+//  Created by Ömer Faruk Kılıçaslan on 19.04.2022.
+//
+
+import XCTest
+
+class SQLite_GirisUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
